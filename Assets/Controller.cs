3af77f6a06e747472caca292250c9a399b775485
Veinsvx <DRshaovx@gmail.com>
2021-11-18 12:34:24 +0800

    1.0

---
+++ Assets/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public Transform[] breakObjs;
+    public Transform[] textInfo;
+    int count = 0;
+
+    public void BreakDownButton()
+    {
+        for (int i = 0; i < breakObjs.Length; i++)
+        {
+            breakObjs[i].GetComponent<Animator>().SetTrigger("Break");
+        } 
+    }
+
+    public void BreakUpButton()
+    {
+        for (int i = 0; i < breakObjs.Length; i++)
+        {
+            breakObjs[i].GetComponent<Animator>().SetTrigger("rest");
+        }
+    }
+
+    public void InfomationOK()
+    {
+        count += 1;
+        if (count == 1)
+        {
+            for (int i = 0; i < textInfo.Length; i++)
+            {
+                textInfo[i].gameObject.SetActive(true);
+            }
+        }
+        else if (count == 2)
+        {
+            for (int i = 0; i < textInfo.Length; i++)
+            {
+                textInfo[i].gameObject.SetActive(false);
+            }
+            count = 0;
+        }
+    }
+
+}
